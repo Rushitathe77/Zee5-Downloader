3c2cfaa2f23d9516f310c7de13ce7d651e808dfc
Rushitathe77 <84010964+Rushitathe77@users.noreply.github.com>
2021-05-13 15:21:31 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -29,7 +29,7 @@ def send_start(bot, update):
     bot.send_message(
         chat_id=update.chat.id,
         text=script.START_TEXT.format(update.from_user.first_name),
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ CHANNEL ⭕️", url="https://t.me/marathirockerzbots")], [InlineKeyboardButton(text="🛑 SUPPORT 🛑", url="https://t.me/TeleRoid14"),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ CHANNEL ⭕️", url="https://t.me/marathirockerzbots")], [InlineKeyboardButton(text="🛑 SUPPORT 🛑", url="https://t.me/joinchat/NNd8EhivSUhkMDhl"),
                                                     InlineKeyboardButton(text="SHARE ♐️", url="tg://msg?text=%2A%2AHai%20%E2%9D%A4%EF%B8%8F%2C%2A%2A%20%0A__Today%20i%20just%20found%20out%20an%20intresting%20and%20Powerful__%20%2A%2AZee5%20Downloader%20Bot%2A%2A%20__for%20Free%F0%9F%A5%B0.__%20%20%0A%2A%2ABot%20Link%20%3A%20%40teleroid_zee5_bot%2A%2A%20%F0%9F%94%A5")]]),
         parse_mode="html",
         disable_web_page_preview=True,
